ee927d4d90dbb3c99d5170eefb11aed73248b1da
KeironO <keiron.oshea@gmail.com>
2019-02-06 18:30:41 +0000

    This is so boring

--- podc/data/video.py
+++ podc/data/video.py
@@ -19,29 +19,44 @@ Boston, MA 02110-1301 USA
 
 import imageio
 from PIL import Image
-import os
+from os.path import join
 import random
-import sys
 from collections import Counter
 from keras.utils import Sequence
-from keras.preprocessing.sequence import pad_sequences
 from keras_preprocessing.image import apply_affine_transform
 from scipy.ndimage.filters import gaussian_filter
 import numpy as np
-from keras import backend as K
 from joblib import Parallel, delayed, cpu_count
 from hurry.filesize import size
 from scipy.signal import convolve2d
-
-import matplotlib.pyplot as plt
-from matplotlib.animation import ArtistAnimation
 
 ROW_AXIS = 0
 COL_AXIS = 1
 CHANNEL_AXIS = 2
 
 class VideoDataGenerator(Sequence):
-    def __init__(self, directory, filenames, labels, batch_size, shuffle=False, height=224, width=224, max_frames=50, optical_flow=False, upsample=False, featurewise_center=False, gaussian_blur=False, rotation_range=False, brightness_range=False, shear_range=False, zoom_range=False, horizontal_flip=False, vertical_flip=False, n_jobs=1):
+    def __init__(
+        self,
+        directory,
+        filenames,
+        labels,
+        batch_size,
+        shuffle=False,
+        height=224,
+        width=224,
+        max_frames=50,
+        optical_flow=False,
+        upsample=False,
+        featurewise_center=False,
+        gaussian_blur=False,
+        rotation_range=False,
+        brightness_range=False,
+        shear_range=False,
+        zoom_range=False,
+        horizontal_flip=False,
+        vertical_flip=False,
+        n_jobs=1
+    ):
         self.directory = directory
         self.filenames = filenames
         self.labels = labels
@@ -63,31 +78,34 @@ class VideoDataGenerator(Sequence):
         self.vertical_flip = vertical_flip
         self.n_jobs = n_jobs
 
-        if self.upsample == True:
+        if self.upsample:
             self._upsample()
+
         self._generate_filepaths()
         self.on_epoch_end()
 
     def _generate_filepaths(self):
-        self.filepaths = [os.path.join(self.directory, x) for x in self.filenames]
+        self.filepaths = [join(self.directory, x) for x in self.filenames]
 
     def _upsample(self):
         # This was written in a fit of extreme tiredness.
-        l = {}
+        tmp_labels = {}
         for fn in self.filenames:
-            l[fn] = self.labels[fn]
-        counts = Counter([x[1] for x in l.items()])
-        smallest_class = min(counts.items(), key=lambda x: x[1]) 
+            tmp_labels[fn] = self.labels[fn]
+        counts = Counter([x[1] for x in tmp_labels.items()])
+        smallest_class = min(counts.items(), key=lambda x: x[1])
         largest_class = max(counts.items(), key=lambda x: x[1])
-        diff = largest_class[1] -  smallest_class[1]
-        
-        smol_boye = [x[0] for x in l.items() if x[1] == smallest_class[0]]
+        diff = largest_class[1] - smallest_class[1]
+
+        smol_boye = [
+            x[0] for x in tmp_labels.items() if x[1] == smallest_class[0]
+            ]
 
         dups = self.filenames.tolist()
 
         for _ in range(diff):
             dups.append(random.choice(smol_boye))
-        
+
         self.filenames = np.array(dups)
 
     def __len__(self):
@@ -104,14 +122,28 @@ class VideoDataGenerator(Sequence):
         filepaths = np.array([self.filepaths[k] for k in indxs])
         X, y = self.__data_generation(filepaths)
 
-        return X, y     
+        return X, y
 
     def __data_generation(self, filepaths):
 
-        def ___random_rotation(x, rg, fill_mode="nearest", cval=0., interpolation_order=1):
+        def ___random_rotation(
+            x,
+            rg,
+            fill_mode="nearest",
+            cval=0.,
+            interpolation_order=1
+        ):
             theta = np.random.uniform(-rg, rg)
             for index in range(x.shape[0]):
-                x[index] = apply_affine_transform(x[index], theta=theta, row_axis=ROW_AXIS, col_axis=COL_AXIS, channel_axis=CHANNEL_AXIS, fill_mode=fill_mode, cval=cval)
+                x[index] = apply_affine_transform(
+                    x[index],
+                    theta=theta,
+                    row_axis=ROW_AXIS,
+                    col_axis=COL_AXIS,
+                    channel_axis=CHANNEL_AXIS,
+                    fill_mode=fill_mode,
+                    cval=cval
+                    )
             return x
 
         def __featurewise_centre(x):
@@ -124,14 +156,23 @@ class VideoDataGenerator(Sequence):
             x = x[::-1, ...]
             x = x.swapaxes(0, axis)
             return x
-            
+
         def __random_shift(x, wrg, hrg, fill_mode="nearest", cval=0.):
 
             tx = np.random.uniform(-hrg, hrg) * x.shape[ROW_AXIS]
             ty = np.random.uniform(-wrg, wrg) * x.shape[COL_AXIS]
 
             for index in range(x.shape[0]):
-                x[index] = apply_affine_transform(x[index], tx=tx, ty=ty, row_axis=ROW_AXIS, col_axis=COL_AXIS, channel_axis=CHANNEL_AXIS, fill_mode=fill_mode, cval=cval)
+                x[index] = apply_affine_transform(
+                    x[index],
+                    tx=tx,
+                    ty=ty,
+                    row_axis=ROW_AXIS,
+                    col_axis=COL_AXIS,
+                    channel_axis=CHANNEL_AXIS,
+                    fill_mode=fill_mode,
+                    cval=cval
+                    )
             return x
 
         def __optical_flow(x, window_size=4, tau=1e-5, mode="same"):
@@ -145,48 +186,78 @@ class VideoDataGenerator(Sequence):
                 kX = np.array([[-1., 1.], [-1., 1.]])
                 kY = np.array([[-1., -1.], [1., 1.]])
                 kT = np.array([[1., 1.], [1., 1.]])
-            
-                w = int(np.floor(window_size/2)) 
-                fxT = np.zeros((self.height, self.width, 3),dtype=float)
-                fyT = np.zeros((self.height,self.width, 3),dtype=float)
-                ftT = np.zeros((self.height,self.width, 3),dtype=float)
+
+                w = int(np.floor(window_size/2))
+                fxT = np.zeros((self.height, self.width, 3), dtype=float)
+                fyT = np.zeros((self.height, self.width, 3), dtype=float)
+                ftT = np.zeros((self.height, self.width, 3), dtype=float)
 
                 for channel_idx in range(3):
-                    fx = convolve2d(frame0[:,:,channel_idx], kX, boundary='symm', mode=mode)
-                    fy = convolve2d(frame0[:,:,channel_idx], kY, boundary='symm', mode=mode)
-                    ft = convolve2d(I2g[:,:,channel_idx], kT, boundary='symm', mode=mode) 
-                    ft += convolve2d(frame0[:,:,channel_idx], -kT, boundary='symm', mode=mode)
+                    fx = convolve2d(
+                        frame0[:, :, channel_idx],
+                        kX,
+                        boundary="symm",
+                        mode=mode
+                        )
 
-                    fxT[:,:,channel_idx] = fx
-                    fyT[:,:,channel_idx] = fy
-                    ftT[:,:,channel_idx] = ft
-                
+                    fy = convolve2d(
+                        frame0[:, :, channel_idx],
+                        kY,
+                        boundary="symm",
+                        mode=mode
+                        )
+
+                    ft = convolve2d(
+                        I2g[:, :, channel_idx],
+                        kT,
+                        boundary="symm",
+                        mode=mode
+                        )
+
+                    ft += convolve2d(
+                        frame0[:, :, channel_idx],
+                        -kT,
+                        boundary="symm",
+                        mode=mode
+                        )
+
+                    fxT[:, :, channel_idx] = fx
+                    fyT[:, :, channel_idx] = fy
+                    ftT[:, :, channel_idx] = ft
+
                 u = np.zeros((self.height, self.width))
                 v = np.zeros((self.height, self.width))
-                
+
                 for i in range(w, frame0.shape[0]-w):
                     for j in range(w, frame0.shape[1]-w):
                         Ix = fxT[i-w:i+w+1, j-w:j+w+1, :].flatten()
                         Iy = fyT[i-w:i+w+1, j-w:j+w+1, :].flatten()
                         It = ftT[i-w:i+w+1, j-w:j+w+1, :].flatten()
-                        b = np.reshape(It, (It.shape[0],1))
+                        b = np.reshape(It, (It.shape[0], 1))
                         A = np.vstack((Ix, Iy)).T
 
-                        nu = np.zeros((2,1))            
-                        if np.min(abs(np.linalg.eigvals(np.matmul(A.T, A)))) >= tau:
+                        nu = np.zeros((2, 1))
+                        if np.min(abs(
+                            np.linalg.eigvals(np.matmul(A.T, A))
+                            )) >= tau:
+
                             nu = np.matmul(np.linalg.pinv(A), b)
-                        
-                        u[i,j]=nu[0]
-                        v[i,j]=nu[1]
+
+                        u[i, j] = nu[0]
+                        v[i, j] = nu[1]
 
                 mask = np.square(u)+np.square(v) == 0.0
                 rawX = x[frame_index]
 
                 for channel_idx in range(3):
-                    channel_X = rawX[:,:,channel_idx]
-                    m = np.ma.masked_array(data=channel_X, mask=mask, fill_value=0.0).filled()
+                    channel_X = rawX[:, :, channel_idx]
+                    m = np.ma.masked_array(
+                        data=channel_X,
+                        mask=mask,
+                        fill_value=0.0
+                        ).filled()
                     rawX[:,:,channel_idx] = m
-                
+
                 x[frame_index] = rawX
 
             return x
@@ -200,19 +271,39 @@ class VideoDataGenerator(Sequence):
         def __random_zoom(x, zoom_range, fill_mode="nearest", cval=0.):
             zx, zy = np.random.uniform(zoom_range[0], zoom_range[1], 2)
             for index in range(x.shape[0]):
-                x[index] = apply_affine_transform(x[index], zx=zx, zy=zy, row_axis=ROW_AXIS, col_axis=COL_AXIS, channel_axis=CHANNEL_AXIS, fill_mode=fill_mode, cval=cval)
+                x[index] = apply_affine_transform(x[index],
+                                                    zx=zx,
+                                                    zy=zy,
+                                                    row_axis=ROW_AXIS,
+                                                    col_axis=COL_AXIS,
+                                                    channel_axis=CHANNEL_AXIS,
+                                                    fill_mode=fill_mode,
+                                                    cval=cval
+                                                    )
             return x
 
         def __random_shear(x, intensity, fill_mode="nearest", cval=0.):
             shear = np.random.uniform(-intensity, intensity)
             for index in range(x.shape[0]):
-                x[index] = apply_affine_transform(x[index], shear=shear, row_axis=ROW_AXIS, col_axis=COL_AXIS, channel_axis=CHANNEL_AXIS, fill_mode=fill_mode, cval=cval)
-            return x              
+                x[index] = apply_affine_transform(
+                    x[index],
+                    shear=shear,
+                    row_axis=ROW_AXIS,
+                    col_axis=COL_AXIS,
+                    channel_axis=CHANNEL_AXIS,
+                    fill_mode=fill_mode,
+                    cval=cval
+                    )
+            return x
 
         def ___read(filepath):
             video = imageio.get_reader(filepath, "ffmpeg")
-            
-            frames = np.empty((self.max_frames, self.width, self.height, 3), dtype="uint8")
+
+            frames = np.empty(
+                    (self.max_frames, self.width, self.height, 3),
+                    dtype="uint8"
+                )
+
             for index, frame in enumerate(video):
                 if index >= self.max_frames:
                     break
@@ -224,60 +315,49 @@ class VideoDataGenerator(Sequence):
                 frame = frame.reshape(frame.shape[0], frame.shape[1], 3)
 
                 frames[index] = frame
-            frame_size = size(sys.getsizeof(frames))
-            logging.info("DATALOAD !! LOADED %s SUCCESS (%s)" % (filepath, frame_size))
+
             return frames
-        
+
         X = np.zeros((len(filepaths), self.max_frames, self.width, self.height, 3), dtype="uint8")
         y = []
 
         def _do(filepath):
             x = ___read(filepath)
-            
+
             if self.featurewise_center != False:
-                logging.info("DATAUG !! FEATUREWISE CENTERING %s" % (filepath))
                 x = __featurewise_centre(x)
-            
+
             if self.gaussian_blur != False:
-                logging.info("DATAUG !! GAUSSIAN BLUR %s" % (filepath))
                 x = __gaussian_blur(x)
 
             if self.rotation_range != False:
-                logging.info("DATAUG !! RANDOM ROTATION %s" % (filepath))
                 x = ___random_rotation(x, self.rotation_range)
-            
+
             if self.horizontal_flip != False:
-                logging.info("DATAUG !! HORIZONTAL FLIP %s" % (filepath))
                 # coinflip
                 if random.randint(0, 1) == 1:
                     x = __flip_axis(x, ROW_AXIS)
-            
+
             if self.vertical_flip != False:
-                logging.info("DATAUG !! VERTICAL FLIP %s" % (filepath))
                 if random.randint(0, 1) == 1:
                     x = __flip_axis(x, COL_AXIS)
 
             if self.shear_range != False:
-                logging.info("DATAUG !! SHEAR %s" % (filepath))
                 x = __random_shear(x, self.shear_range)
 
             if self.brightness_range != False:
                 pass
 
             if self.zoom_range != False:
-                logging.info("DATAUG !! HORIZONTAL FLIP %s" % (filepath))
                 x = __random_zoom(x, self.zoom_range)
 
-            if self.optical_flow != False:   
-                logging.info("DATAUG !! OPTICAL FLOW %s" % (filepath))
+            if self.optical_flow != False:
                 x = __optical_flow(x, window_size=self.optical_flow)
-
-            logging.info("RETURNING !! DATA %s (%s)" % (filepath, size(sys.getsizeof(x))))
 
             return x, self.labels["".join(os.path.splitext(os.path.basename(filepath)))]
 
 
-    
+
         if self.n_jobs == -1:
             self.n_jobs = cpu_count()
         if self.n_jobs > 1:
